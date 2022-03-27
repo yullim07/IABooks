5fbf1d06eba15d1ba2e743db31e98c70c76ba6cd
Jun <Jun@DESKTOP-HKJD0JV>
2022-03-27 19:02:40 +0900

    커맨드

---
+++ IABooks/src/main/java/common/controller/IndexController.java
@@ -0,0 +1,25 @@
+package common.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class IndexController extends AbstractController {
+
+	@Override
+	public String toString() {
+		return "확인용 IndexController 클래스의 인스턴스 메소드인 toString호출";
+	}
+	
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+	
+		//super.setRedirect(false);//false 생략가능
+		//this.setRedirect(false); 다같다.
+		//setRedirect(false);
+		
+		super.setViewPage("/WEB-INF/index.jsp");
+	}
+
+}
---
+++ IABooks/src/main/java/util/security/SecretMyKey.java
@@ -0,0 +1,6 @@
+package util.security;
+
+public class SecretMyKey {
+	public final static String KEY = "abcd0070#cclass$"; //글자수 16이상 넘어야 오류가없다 UnsupportedEncodingException
+	
+}
