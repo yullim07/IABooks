79034f0700f105d8e111063d8d96f5197b120f23
sist <sist@Jeong>
2022-03-29 22:06:49 +0900

    게시판 합본 commit

--- IABooks/src/main/java/board/model/InterBoardDAO.java
+++ IABooks/src/main/java/board/model/InterBoardDAO.java
@@ -7,6 +7,7 @@ public interface InterBoardDAO {
 
 	List<QnABoardVO> boardList();// 글목록보기
 	
+	List<ReviewBoardVO> reviewList(); //리뷰글목록보기
 	
 	// FAQ 글목록보기
 	List<FaqBoardVO> faqBoardList() throws SQLException;
