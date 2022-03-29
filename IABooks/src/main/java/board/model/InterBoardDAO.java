9c3dbde1c91592a27c8420b25c5bd1b296c916e2
sist <sist@Jeong>
2022-03-30 01:05:52 +0900

    interboardDAO 동기화

--- IABooks/src/main/java/board/model/InterBoardDAO.java
+++ IABooks/src/main/java/board/model/InterBoardDAO.java
@@ -6,14 +6,14 @@ import java.util.Map;
 
 public interface InterBoardDAO {
 
-	List<QnABoardVO> boardList();// 글목록보기
-	
-	List<ReviewBoardVO> reviewList(); //리뷰글목록보기
-	
-	// FAQ 글목록보기
-	List<FaqBoardVO> faqBoardList() throws SQLException;
+   List<QnABoardVO> boardList();// 글목록보기
+   
+   List<ReviewBoardVO> reviewList() throws SQLException; //리뷰글목록보기
+   
+   // FAQ 글목록보기
+   List<FaqBoardVO> faqBoardList() throws SQLException;
 
-	// FAQ 게시판에 글 작성하기
-	int writeFaqBoard(Map<String, String> paraMap) throws SQLException;
+   // FAQ 게시판에 글 작성하기
+   int writeFaqBoard(Map<String, String> paraMap) throws SQLException;
 
 }
