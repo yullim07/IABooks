ab16c5fdec45ec9c12829bf569c615226f01b1e4
sist <sist@LAPTOP-BJURLHTH>
2022-03-31 15:53:48 +0900

    커밋

--- IABooks/src/main/webapp/WEB-INF/SQL/IABooks_moonby.sql
+++ IABooks/src/main/webapp/WEB-INF/SQL/IABooks_moonby.sql
@@ -7,8 +7,6 @@ from tabs;
 select *
 from tbl_member;
 
-insert into tbl_member(PK_USERID,PWD,NAME,ADDRESS,DETAILADDRESS,UQ_PHONE,UQ_EMAIL,POSTCODE)
-values('leess','qwer1234$','이순신','조선','함경도','01012345678','leess@gmail.com','15441');
 
 
 
