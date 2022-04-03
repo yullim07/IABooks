b18e3ad563e4984f7406fd72f5c9aa1ee29d24fb
Jun <Jun@DESKTOP-HKJD0JV>
2022-04-03 14:25:58 +0900

    2조

---
+++ IABooks/src/main/java/product/controller/CartAction.java
@@ -0,0 +1,131 @@
+package product.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import common.controller.AbstractController;
+import member.model.InterMemberDAO;
+import member.model.MemberDAO;
+import member.model.MemberVO;
+import product.model.CartVO;
+import product.model.InterProductDAO;
+import product.model.ProductDAO;
+import product.model.ProductVO;
+
+public class CartAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String method = request.getMethod();
+		
+		/*
+		if(!"POST".equalsIgnoreCase(method)) { // POST 방식이 아닌 경우
+			String message = "잘못된 경로입니다!";
+			String loc = "javascript:history.back()"; // 이전 페이지로 가는 것
+			
+			request.setAttribute("message", message); // requset 영역에 넣어주기 
+			request.setAttribute("loc", loc);
+			
+		//	super.setRedirect(false);
+			super.setViewPage("/WEB-INF/msg.jsp");
+		}
+		*/
+		
+	//	else {
+			
+			HttpSession session = request.getSession();
+			MemberVO loginuser = (MemberVO)session.getAttribute("loginuser");
+			
+			// 로그인하지 않은 경우
+			if(loginuser == null) {
+				String message = "로그인을 해주세요!";
+				String loc = "javascript:history.back()"; // 이전 페이지로 가는 것
+				
+				request.setAttribute("message", message); // requset 영역에 넣어주기 
+				request.setAttribute("loc", loc);
+				
+			//	super.setRedirect(false);
+				super.setViewPage("/WEB-INF/msg.jsp");
+			}
+			
+			// 로그인한 유저인 경우
+			else {
+				String userid = loginuser.getUserid();
+				
+				InterProductDAO pdao = new ProductDAO();
+				List<CartVO> cartList = pdao.getCartList(userid);
+				
+				request.setAttribute("cartList", cartList);
+				
+			//	super.setRedirect(false);
+				super.setViewPage("/WEB-INF/product/cart.jsp");
+			}
+	//	}
+	} // end of execute ----------------------------------
+	
+	
+		
+	/*
+		String method = request.getMethod();
+		
+		if(!"POST".equalsIgnoreCase(method)) {
+			
+		}
+		
+		else { // POST 방식으로 넘어온 경우
+			
+			String userid = request.getParameter("userid");
+			String pwd = request.getParameter("pwd");
+			String pk_pro_num = request.getParameter("pk_pro_num");
+			
+			// Map에 넣어주기
+			Map<String, String> paraMap = new HashMap<>();
+			paraMap.put("userid", userid);
+			paraMap.put("pwd", pwd);
+			paraMap.put("pk_pro_num", pk_pro_num);
+			
+			InterProductDAO pdao = new ProductDAO();
+			ProductVO productInfo = pdao.showBookDetail(pk_pro_num);
+			InterMemberDAO mdao = new MemberDAO();
+			MemberVO loginuser = mdao.selectOneMember(paraMap);
+			
+			if(loginuser != null) { // 로그인 유저의 정보가 존재하는 경우
+				HttpSession session = request.getSession();
+				// [암기] 메모리에 생성되어있는 session을 불러오는 것이다.
+				
+				session.setAttribute("loginuser", loginuser);
+				// session(세션)에 로그인된 사용자 정보인 loginuser 를 키이름을 "loginuser"로 저장시키는 것이다.
+				
+				// 페이지 이동
+				super.setRedirect(true); // true면 페이지 이동
+				
+				// 로그인을 하면 시작페이지(index.up)로 가는 것이 아니라 로그인을 시도하려고 머물렀던 그 페이지로 가기 위한 것이다.
+				// 네이버처럼! 예를 들어 자바스크립트를 검색하다 로그인하면 로그인 상태로 변경되고 보고있던 페이지에 계속 머무는 것
+				// 나증에 할 것이다!
+				
+				super.setViewPage(request.getContextPath()+"/index.up");
+				
+			}
+			else { // 로그인 유저의 정보가 존재하지 않는 경우
+				
+				String message = "비회원은 장바구니 기능을 사용하실 수 없습니다!";
+				String loc = "javascript:history.back()"; // [암기!] 이전 페이지
+				
+				request.setAttribute("message", message); // requset 영역에 넣어주기 
+				request.setAttribute("loc", loc);
+				
+			//	super.setRedirect(false);
+				super.setViewPage(request.getContextPath()+"/index.up");
+			}
+		}
+	//	super.setRedirect(false);
+		super.setViewPage("/WEB-INF/product/cart.jsp");
+	}*/
+
+}
